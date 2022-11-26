20ffd177c3a913add636be5d41ca559d1259505e
Miguel B. Henriques <103072223+MiguelHenri@users.noreply.github.com>
2022-11-25 22:35:56 -0300

    Update sdc.sol

--- codes/sdc.sol
+++ codes/sdc.sol
@@ -6,8 +6,10 @@ pragma solidity >=0.7.0 <0.9.0;
 contract SDC {
     
     struct objection {
+        address o_owner;
         string o_string;
         uint votes;
+        bool exist;
     }
 
     struct affirmation {
@@ -19,11 +21,15 @@ contract SDC {
         uint current_objections;
     }
 
+    //addresses (users) will have both data
+    mapping(address => bool) already_voted;
+    mapping(address => uint) statement_voted;
+
     address public owner;
     uint public createdAt;
     affirmation new_a; //contract main affirmation
 
-    constructor(){
+    constructor() { 
         owner = msg.sender;
         createdAt = block.timestamp;
         new_a.votes = 0; //start with no votes
@@ -42,29 +48,51 @@ contract SDC {
         _;
     }
 
+    modifier not_owner() {
+        require(msg.sender != owner, "own main affirmation");
+        _;
+    }
+
+    modifier objection_exist(uint _num) {
+        require(new_a.objections[_num].exist == true, "objection does not exist");
+        _;
+    }
+
+    modifier can_vote(uint _num) {
+        require((statement_voted[msg.sender] < _num)||(already_voted[msg.sender]==false), "cannot vote for previus statement");
+        _;
+    }
+
     function set_affirmation(string memory _s) external
     can_set_affirmation(new_a.received_string) is_owner() {
         new_a.received_string = true; //will not accept string changes
         new_a.a_string = _s;
     }
 
-    function get_affirmation() external view returns(string memory){
-        return new_a.a_string; //returns main affirmation
+    function get_statement(uint _num) external view 
+    objection_exist(_num) returns(string memory) {
+        if(_num == 0){
+            return new_a.a_string; //returns main affirmation
+        }
+        else{
+            return new_a.objections[_num].o_string; //returns objection[_num]
+        }
     }
 
-    function vote(uint _num) public {
-        /*will require that [_num] objections exists*/
-        /*will require if user already voted in any objection/affirmation in this contract*/
+    function vote(uint _num) public 
+    objection_exist(_num) can_vote(_num) {
         if(_num == 0){
             new_a.votes += 1;
         }
         else{
             new_a.objections[_num].votes += 1;
         }
+        already_voted[msg.sender] = true;
+        statement_voted[msg.sender] = _num;
     }
 
-    function get_votes(uint _num) external view returns(uint){
-        /*will require that [_num] objections exists*/
+    function get_votes(uint _num) external view
+    objection_exist(_num) returns(uint) { 
         if(_num == 0){
             return new_a.votes;
         }
@@ -73,4 +101,25 @@ contract SDC {
         }
     }
 
+    function current_objections() external view returns(uint) {
+        return new_a.current_objections;
+    }
+
+    function create_objection(string memory _s) public 
+    not_owner() {
+        new_a.current_objections += 1;
+        new_a.objections[new_a.current_objections].o_owner = msg.sender;
+        new_a.objections[new_a.current_objections].o_string = _s;
+        new_a.objections[new_a.current_objections].votes = 0;
+        new_a.objections[new_a.current_objections].exist = true;
+    }
+
+    function delete_objection(uint _num) private
+    objection_exist(_num) {
+        new_a.current_objections -= 1;
+        new_a.objections[_num].o_owner = address(0);
+        new_a.objections[_num].o_string = "";
+        new_a.objections[_num].votes = 0;
+        new_a.objections[_num].exist = false;
+    }
 }
